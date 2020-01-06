d7269a30ca2cf74c7f70a1c95544565e4a3cc5e2
yaffa-yasso <yaffa2411@gmail.com>
2020-01-06 13:06:54 +0200

    writing README.md

--- Print.cpp
+++ Print.cpp
@@ -8,20 +8,30 @@ Print::Print(unordered_map<string, Var> &varProgram)  {
 }
 
 int Print:: execute(vector<string> &v){
-  string s = v[1];
-  //if it's a string
-  if(s[0] == '"'){
-    cout  << v[1] <<endl;
-    //else -> if the variable exist in varProgram->insert it to interpreter's map.
-  } else if((*varProgram).find(s) == (*varProgram).end()){
-    // not exist -> need to throw exception
-  }else{
-    Interpreter i;
-    insertToMap((*varProgram),i.GetVariablesMap());
-//    cout << " rpm + " <<varProgram["rpm"].GetSim()<<endl;
-    Expression *e = i.interpret(s);
-//    cout<< e->calculate() << endl;
-  }
+    string s = v[1];
+    //if it's a string
+    if(s[0] == '"'){
+        int i = 1;
+        char end = s[s.length() - 1];
+        while (end != '"'){
+            i++;
+            s += " " + v[i];
+            end = s[s.length() - 1];
+        }
+        cout << s << endl;
+        return i + 1;
+
+        //else -> if the variable exist in varProgram->insert it to interpreter's map.
+    } else {
+        if ((*varProgram).find(s) == (*varProgram).end()) {
+            // not exist -> need to throw exception
+        } else {
+            Interpreter i;
+            insertToMap((*varProgram), i.GetVariablesMap());
+            Expression *e = i.interpret(s);
+            cout<< e->calculate() << endl;
+        }
+    }
 
 
 
